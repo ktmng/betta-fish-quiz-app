98ac503c6a7dbb6d3adb438816d91d3e5c3de9db
Kate Meng <katemeng08@gmail.com>
2020-03-12 00:36:53 -0700

    no console.log msgs, proper callbackfxn syntax, display correct ans

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const database = [
     answer2: 'Gamma Fish',
     answer3: 'Siamese Fighting Fish', 
     answer4: 'Kissing Gouramis',
-    correctAnswer: 'Siamese Fighting Fish'
+    correctAnswer: 'Siamese Fighting Fish',
+    explanation: 'Bettas were historically bred in Southeast Asian countries, like Thailand and Myanmar, specifically to increase aggressivity so that they would last longer during matches against each other. Thailand was formerly known as Siam.'
   },
   {
     question: 'On average, how long do betta fish live in captivity?',
@@ -15,7 +16,8 @@ const database = [
     answer2: '3-5 years',
     answer3: '6-9 years',
     answer4: '10+ years',
-    correctAnswer: '3-5 years'
+    correctAnswer: '3-5 years',
+    explanation: 'Typically, betta fish sold in pet stores are already around a year old. This is because it takes a while for betta fish to fully develop their colors, which is what makes them attractive pets for most people. With proper care, you’re likely to extend the life of your betta!'
   },
   {
     question: 'Betta fish are bred around the world for their amazing colors and diverse fins! They are often identified and referred to by the shape of their tails. Which of the following is not a common betta tail type?',
@@ -23,7 +25,8 @@ const database = [
     answer2: 'Veiltail',
     answer3: 'Spadetail',
     answer4: 'Eclipsetail',
-    correctAnswer: 'Eclipsetail'
+    correctAnswer: 'Eclipsetail',
+    explanation: 'Although it would be cool to see a betta to have a fully circular tail like an eclipse, they do not exist. However, feathertail, veiltail, and spadetail bettas do exist and are all aptly named!'
   }, 
   {
     question: 'What is the name of the nests that male betta fish create at the surface of the water?',
@@ -31,7 +34,8 @@ const database = [
     answer2: 'Bubble Nests',
     answer3: 'Baby Betta Nests', 
     answer4: 'Floating Nests',
-    correctAnswer:'Bubble Nests'
+    correctAnswer:'Bubble Nests',
+    explanation: 'Male betta fish are meant to build bubble nests during courtship. That’s so they can scoop the eggs laid by the female with their mouths and spit them into the nest. Though, male bettas can also be seen building them without a mate present.'
   },
   {
     question: 'What organ allows betta fish to breathe air and survive in waters with low-oxygen content and out of water for short periods?',
@@ -39,7 +43,8 @@ const database = [
     answer2: 'Gills',
     answer3: 'Labyrinth Organ',
     answer4: 'Air Sac Organ',
-    correctAnswer: 'Labyrinth Organ'
+    correctAnswer: 'Labyrinth Organ',
+    explanation: 'Just because they can live in low-oxygen environments, doesn’t mean they should! Keep your water well oxygenated!'
   },
   {
     question: 'What kind of container should betta fish be kept in if you were to house one?',
@@ -47,7 +52,8 @@ const database = [
     answer2: 'A vase with a live plant that’s kept in the kitchen',
     answer3: 'A fishbowl like the one Dorothy has on Elmo’s World',
     answer4: 'A tank that is at least 2.5 gallons',
-    correctAnswer: 'A tank that is at least 2.5 gallons'
+    correctAnswer: 'A tank that is at least 2.5 gallons',
+    explanation: 'This is the minimum size tank a betta should be placed in! A small aquarium also requires more frequent water changes and tank cycling since toxins build up faster in them. Also, keep in mind how many decorations you have, as that may crowd your tank giving less space to swim.'
   },
   {
     question: 'Betta fish are tropical fish, naturally found in the warm waters at lower altitudes. What water temperatures should betta fish be kept in?',
@@ -55,7 +61,8 @@ const database = [
     answer2: '72-75°F (22.2-25.0°C)',
     answer3: '78-80°F (25.6-26.5°C)',
     answer4: '81-86°F (27.2-30.0°C)',
-    correctAnswer: '78-80°F (25.6-26.5°C)'
+    correctAnswer: '78-80°F (25.6-26.5°C)',
+    explanation: 'This means that you’ll most likely have to get an aquarium heater as room temperature water is often too cold for betta fish. Cold water will slow down your betta’s metabolism, cause it to be lethargic, and increase its susceptibility to disease.'
   },
   {
     question: 'Although typically an agressive species, it is possible for betta fish to have tank mates. However, you have to be careful especially when introducing them. Which tank mates would be an appropriate choice for betta fish?',
@@ -63,7 +70,8 @@ const database = [
     answer2: 'Cichlids',
     answer3: 'Schooling Fish',
     answer4: 'Corydoras Catfish',
-    correctAnswer: 'Corydoras Catfish'
+    correctAnswer: 'Corydoras Catfish',
+    explanation: 'Corydoras catfish have a calm, peaceful and non-aggressive temperament, which makes them good tank mates for most fish. It’s recommended that your betta fish is introduced last to a community tank. Make sure to monitor its behavior when introduced and always have a back-up plan in case things don’t work out!'
   },
   {
     question: 'What tank decor is inappropriate for betta fish?',
@@ -71,7 +79,8 @@ const database = [
     answer2: 'Marimo Balls',
     answer3: 'Live Plants',
     answer4: 'Smooth Rocks',
-    correctAnswer: 'Seashells and Coral'
+    correctAnswer: 'Seashells and Coral',
+    explanation: 'Seashells and coral contain calcium not needed in freshwater tanks. When the calcium is released into the water, it alters the pH, making it harder to maintain stable water conditions.'
   }, 
   {
     question: 'Betta fish love to eat and beg for more food! Be careful not to overfeed them! Which of the following is least appropriate for betta fish to consume?',
@@ -79,7 +88,8 @@ const database = [
     answer2: 'Betta Pellets',
     answer3: 'Blood Worms',
     answer4: 'Brine Shrimp',
-    correctAnswer: 'Fish Flakes'
+    correctAnswer: 'Fish Flakes',
+    explanation: 'Betta fish are carnivorous, meaning they require more protein in their diet. That’s why options like specially made betta pellets, blood worms, and brine shrimp are good choices! Often fish will swallow air at the surface of the water when eating fish flakes and other floating foods, causing bloating. Some fishkeepers recommend soaking the food in water first before feeding to remedy this issue.'
   }, 
 ];
   
@@ -93,7 +103,6 @@ const STORE = {
 
 //q:0 score:0-------------------------------------------------------------------------------V
 function generateZeroQuestionAndScore() {
-  console.log('`generateZeroQuestionAndScore` ran')
   return`
   <section class='currentQuestionAndScore'>
     <ul>
@@ -106,7 +115,6 @@ function generateZeroQuestionAndScore() {
 
 //starting screen---------------------------------------------------------------------------V
 function generateStartingScreen() { 
-  console.log('`generateStartingScreen` ran')
   return `
   <section class="startBox">
     <span class="quizTitle">Betta Fish Quiz</span>
@@ -116,7 +124,6 @@ function generateStartingScreen() {
 } 
 
 function renderStartingScreen() {
-  console.log('`renderStartingScreen` ran')
   const addToHtmlAlt = generateZeroQuestionAndScore();
   $('.quizState').html(addToHtmlAlt);
   const addToHtml = generateStartingScreen();
@@ -126,7 +133,6 @@ function renderStartingScreen() {
 
 //question and score updater and zeroer-----------------------------------------------------V
 function generateQuestionAndScoreUpdater() {
-  console.log('`generateQuestionAndScoreUpdater` ran')
   return`
   <section class='currentQuestionAndScore'>
     <ul>
@@ -137,7 +143,6 @@ function generateQuestionAndScoreUpdater() {
 }
 
 function renderQuestionAndScoreUpdater() {
-  console.log('`renderQuestionAndScoreUpdater` ran')
   const addToHtml = generateQuestionAndScoreUpdater();
   const addToHtmlAlt = generateZeroQuestionAndScore();
 
@@ -151,7 +156,6 @@ function renderQuestionAndScoreUpdater() {
 
 //question----------------------------------------------------------------------------------V
 function generateQuestion (question, answer1, answer2, answer3, answer4) {
-  console.log('`generateQuestion` ran')
   return`
   <section class="questionBox">
     <form>
@@ -168,7 +172,6 @@ function generateQuestion (question, answer1, answer2, answer3, answer4) {
 }
 
 function renderQuestion() { //arrayName[item#]
-  console.log('`renderQuestion` ran')
   const question = database[STORE.questionNumber].question,
   answer1 = database[STORE.questionNumber].answer1, 
   answer2 = database[STORE.questionNumber].answer2,
@@ -181,19 +184,18 @@ function renderQuestion() { //arrayName[item#]
 
 //correct screen----------------------------------------------------------------------------V
 function generateCorrectAnswer() {
-  console.log('`generateCorrectAnswer` ran')
   return `
   <section class="responseBox">
     <section class='correctAnswer'>
       <span class='responseText'>That's Correct!</span>
       <img src="images/betta open mouth.jpg" alt="A betta fish with its mouth open" class="betta-reaction-img">
+      <span class='explanation'>${database[STORE.questionNumber].explanation}</span>
       <button class='nextButton'>Next Question</button>
     </section>
   </section> `;
 }
 
 function renderCorrectAnswer() {
-  console.log('`renderCorrectAnswer` ran')
   const addToHtml = generateCorrectAnswer();
   $('.main').html(addToHtml);
 }
@@ -201,19 +203,18 @@ function renderCorrectAnswer() {
 
 //wrong screen------------------------------------------------------------------------------V
 function generateWrongAnswer() {
-  console.log('`generateCorrectAnswer` ran')
   return `
   <section class="responseBox">
     <section class='wrongAnswer'>
       <span class='responseText'>Sorry, That's Not Right</span>
       <img src="images/betta flared.jpg" alt="A betta fish flaring or expanding its gill plates" class="betta-reaction-img">
+      <span class='actualAnswer'>Correct Answer: ${database[STORE.questionNumber].correctAnswer}</span>
       <button class='nextButton'>Next Question</button>
     </section> 
   </section> `;
 }
 
 function renderWrongAnswer() {
-  console.log('`renderWrongAnswer` ran')
   const addToHtml = generateWrongAnswer();
   $('.main').html(addToHtml);
 }
@@ -221,7 +222,6 @@ function renderWrongAnswer() {
 
 //final score screen------------------------------------------------------------------------V
 function generateFinalScore() {
-  console.log('`generateFinalScore` ran')
   let results = '';
     if (STORE.currentScore >= 10) {
       results = 'Wow! You got them all!';
@@ -242,7 +242,6 @@ function generateFinalScore() {
 }
 
 function renderFinalScore() {
-  console.log('`renderFinalScore` ran')
   const addToHtml = generateFinalScore();
   $('.main').html(addToHtml);
 }
@@ -254,7 +253,6 @@ function renderFinalScore() {
 
 //start button------------------------------------------------------------------------------V
 function handleStartButton() {
-  console.log('`handleStartButton` ran');
 	$('.main').on('click', '.startButton', () => {
 		STORE.quizOnOff = 'on';
     renderQuestionAndScoreUpdater();
@@ -265,7 +263,6 @@ function handleStartButton() {
 
 //submit button------------------------------------------------------------------------------V
 function handleSubmitButton() {
-  console.log('`handleSubmitButton` ran')
   $('.main').submit('.submitButton', () => {
     event.preventDefault();
 
@@ -285,7 +282,6 @@ function handleSubmitButton() {
 
 //next button--------------------------------------------------------------------------------V
 function handleNextButton() {
-  console.log('`handleSubmitButton` ran')
 	$('.main').on('click', '.nextButton', () => {
 		if (STORE.questionNumber >= 9) {
 			renderFinalScore();
@@ -312,12 +308,12 @@ function handleRetakeButton() {
 
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^     HANDLE EVENTS ABOVE      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-// TESTING!!!
-function testing() {
+// initialzie app vvvv
+function initApp() {
   renderStartingScreen();
   handleStartButton();
   handleSubmitButton();
   handleNextButton();
   handleRetakeButton();
 }
-testing();
+$(initApp);
